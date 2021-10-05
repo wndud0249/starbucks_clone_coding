389dae62816b834e867a25abfba43db6ccf535fa
jeongjuyoung <jjyfm@promotion.co.kr>
2021-10-05 09:17:46 +0900

    회사에서 커밋

--- js/main.js
+++ js/main.js
@@ -69,3 +69,20 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next',
   },
 });
+
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
